ea57d4905561511175da3392ccc54d9bec77eb58
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2024-03-16 14:48:30 +0300

    Dodger moves

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+function moveDodgerLeft(){
+    const dodger = document.getElementById("dodger");
+    dodger.style.left = "0px";
+
+}
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(dodger.style.left.replace('px', ''), 10);
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
